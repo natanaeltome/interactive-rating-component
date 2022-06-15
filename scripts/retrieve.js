08797b259e2919bf205dffaa800e8d3fb138d5da
Natanael <natanaelbandeira@gmail.com>
2022-06-15 16:48:00 -0600

    added second page

--- scripts/retrieve.js
+++ scripts/retrieve.js
@@ -2,7 +2,7 @@ function loadData() {
     console.log(sessionStorage.getItem("ratingValue"));
     let checkedRadioValue = sessionStorage.getItem("ratingValue");
     let paragraph = document.getElementById("final");
-    let text = document.createTextNode(`Rating of ${checkedRadioValue} out of 5`);
+    let text = document.createTextNode(`You selected ${checkedRadioValue} out of 5`);
     paragraph.appendChild(text);
 
     sessionStorage.removeItem("CheckedRadioValue");
