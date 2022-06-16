badd01e7c9e64b8c18d6419a26aea6897ac34621
Natanael <natanaelbandeira@gmail.com>
2022-06-16 12:26:04 -0600

    improved readability of the code

--- scripts/retrieve.js
+++ scripts/retrieve.js
@@ -1,9 +1,8 @@
 function loadData() {
-    console.log(sessionStorage.getItem("ratingValue"));
-    let checkedRadioValue = sessionStorage.getItem("ratingValue");
-    let paragraph = document.getElementById("final");
-    let text = document.createTextNode(`You selected ${checkedRadioValue} out of 5`);
+    let rating = sessionStorage.getItem("ratingValue");
+    let paragraph = document.getElementById("result");
+    let text = document.createTextNode(`You selected ${rating} out of 5`);
     paragraph.appendChild(text);
 
-    sessionStorage.removeItem("CheckedRadioValue");
+    sessionStorage.removeItem("ratingValue");
 };
