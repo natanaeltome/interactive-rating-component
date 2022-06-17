f14b93063cac666c572bee8c78e27bbebec6fa98
Natanael <natanaelbandeira@gmail.com>
2022-06-17 16:10:26 -0600

    Improved data script by adding a Event Listener instead of multiple conditionals

--- scripts/data.js
+++ scripts/data.js
@@ -1,11 +1,8 @@
 function storeData() {
-    let rating = -1;
+    const form = document.getElementById('rating');
 
-    if (document.getElementById("opt1").checked) rating = 1;
-    if (document.getElementById("opt2").checked) rating = 2;
-    if (document.getElementById("opt3").checked) rating = 3;
-    if (document.getElementById("opt4").checked) rating = 4;
-    if (document.getElementById("opt5").checked) rating = 5;
-
-    sessionStorage.setItem("ratingValue", rating);
+    form.addEventListener('submit', (event) => {
+        const rating = document.querySelector('input[name=feedback]:checked').value;
+        sessionStorage.setItem("ratingValue", rating);
+    });
 }
